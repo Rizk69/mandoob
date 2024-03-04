988eed0b2690fb72e725cfeb123724fb5d77e7bf
mostafa 3tta <mostafatop985@gmail.com>
2024-03-04 09:40:17 +0200

    Optimize App

--- lib/data/network/app_api.g.dart
+++ lib/data/network/app_api.g.dart
@@ -44,30 +44,6 @@ class _AppServiceClient implements AppServiceClient {
     return value;
   }
 
-  @override
-  Future<RegisterResponse> register(registerRequest) async {
-    const _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(registerRequest.toJson());
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<RegisterResponse>(Options(
-      method: 'POST',
-      headers: _headers,
-      extra: _extra,
-    )
-            .compose(
-              _dio.options,
-              'api/APIUsers/Register',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = RegisterResponse.fromJson(_result.data!);
-    return value;
-  }
-
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
