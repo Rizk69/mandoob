b47e9112d1303ab015eb7ccf0d43bd9f7977fb7a
mostafa 3tta <mostafatop985@gmail.com>
2024-03-13 04:49:41 +0200

    Finish Profile

--- lib/presentation/profile/presentation/profile_view.dart
+++ lib/presentation/profile/presentation/profile_view.dart
@@ -20,142 +20,156 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: ColorManager.backGround,
-      drawer: buildDrawer(context),
-      body: BlocProvider<ProfileCubit>(
-          create: (_) => instance<ProfileCubit>()..getProfile(),
-          child: BlocBuilder<ProfileCubit, ProfileState>(
-            builder: (context, state) {
-              if (state is ProfileLoadingState) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is ProfileLoadedState) {
-                var user = ProfileCubit.get(context).userModel?.user;
-                return SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 18),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: AppSize.s4.h,
-                        ),
-                        HeaderScreen(
-                            functionDrawer: () {
-                              scaffoldKey.currentState?.openDrawer();
-                            },
-                            title: 'الحساب',
-                            functionIcon: () {
-                              Navigator.pop(context);
-                            }),
-                        SizedBox(height: AppSize.s5.h),
-                        Container(
-                          padding: EdgeInsets.all(AppPadding.p45),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: ColorManager.baseColorLight,
-                              border: Border.all(
-                                  color: ColorManager.baseColorLight,
-                                  width: 5,
-                                  strokeAlign: AppSize.s5)),
-                          child: SvgPicture.asset(
-                            'assets/images/edit_icons.svg',
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: ColorManager.backGround,
+        drawer: buildDrawer(context),
+        body: BlocProvider<ProfileCubit>(
+            create: (_) => instance<ProfileCubit>()..getProfile(),
+            child: BlocBuilder<ProfileCubit, ProfileState>(
+              builder: (context, state) {
+                if (state is ProfileLoadingState) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is ProfileLoadedState) {
+                  var user = ProfileCubit.get(context).userModel?.user;
+                  return SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 18),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: AppSize.s6.h,
                           ),
-                        ),
-                        SizedBox(height: AppSize.s5.h),
-                        Text(
-                          user!.name,
-                          style: getBoldOxygenStyle(
-                            color: ColorManager.black,
-                            fontSize: 23,
+                          HeaderScreen(
+                              functionDrawer: () {
+                                scaffoldKey.currentState?.openDrawer();
+                              },
+                              title: 'الحساب',
+                              functionIcon: () {
+                                Navigator.pop(context);
+                              }),
+                          SizedBox(height: AppSize.s5.h),
+                          Container(
+                            padding: EdgeInsets.all(AppPadding.p45),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: ColorManager.baseColorLight,
+                                border: Border.all(
+                                    color: ColorManager.baseColorLight,
+                                    width: 5,
+                                    strokeAlign: AppSize.s5)),
+                            child: SvgPicture.asset(
+                              'assets/images/edit_icons.svg',
+                            ),
                           ),
-                        ),
-                        SizedBox(height: AppSize.s5.h),
-                        customCardProfile(
-                            color: ColorManager.grey2,
-                            title: 'اسم المستخدم',
-                            des: user.email),
-                        customCardProfilePassword(
-                            color: ColorManager.transparent,
-                            title: 'كلمة السر',
-                            des: '#############33'),
-                        customCardProfile(
-                            color: ColorManager.grey2,
-                            title: 'الرصيد بالعملة',
-                            des: '23671467'),
-                        customCardProfile(
-                            color: ColorManager.transparent,
-                            title: 'كلمة السر',
-                            des: '#############33'),
-                        customCardProfile(
-                            color: ColorManager.grey2,
-                            title: 'اجمالي المبيعات للشهر',
-                            des: "${user.balanceTL}"),
-                        customCardProfile(
-                            color: ColorManager.transparent,
-                            title: 'اجمالي الكميات المباعة',
-                            des: '172637'),
-                        customCardProfile(
-                            color: ColorManager.grey2,
-                            title: 'عمولات الشهر',
-                            des: '172637'),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 10),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
+                          SizedBox(height: AppSize.s5.h),
+                          Text(
+                            user!.name,
+                            style: getBoldOxygenStyle(
+                              color: ColorManager.black,
+                              fontSize: 23,
+                            ),
+                          ),
+                          SizedBox(height: AppSize.s5.h),
+                          customCardProfile(
+                              color: ColorManager.grey2,
+                              title: 'اسم المستخدم',
+                              des: user.email),
+                          customCardProfilePassword(
                               color: ColorManager.transparent,
-                              borderRadius: BorderRadius.circular(15)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              RichText(
-                                text: TextSpan(
-                                  style: getBoldSegoeStyle(
-                                    fontSize: 25,
-                                    color: ColorManager.black,
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: 'المظهر \t\t\t\t',
+                              title: 'كلمة السر',
+                              des: '#############33'),
+                          customCardProfile(
+                              color: ColorManager.grey2,
+                              title: 'كلمة السر',
+                              des: '#############33'),
+                          customCardProfile(
+                              color: ColorManager.transparent,
+                              title: 'الرصيد بالليرة',
+                              des: user.balanceTL.toString()),
+                          customCardProfile(
+                              color: ColorManager.grey2,
+                              title: 'الرصيد بالدولار',
+                              des: user.balanceUsd.toString()),
+
+                          customCardProfile(
+                              color: ColorManager.transparent,
+                              title: 'اجمالي المبيعات للشهر بالليرة',
+                              des: user.salseLera.toString()),
+                          customCardProfile(
+                              color: ColorManager.grey2,
+                              title: 'اجمالي الكميات بالدولار',
+                              des: user.salseDoler.toString()),
+
+                          customCardProfile(
+                              color: ColorManager.transparent,
+                              title: ' عمولات الشهر بالليرة',
+                              des: user.commissionsLera.toString()),
+
+                          customCardProfile(
+                              color: ColorManager.grey2,
+                              title: 'عمولات الشهر بالدولار',
+                              des: user.commissionsDoler.toString()),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 10),
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: ColorManager.transparent,
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    style: getBoldSegoeStyle(
+                                      fontSize: 25,
+                                      color: ColorManager.black,
                                     ),
-                                    TextSpan(
-                                      text: 'light',
-                                      style: getMediumInterStyle(
-                                        fontSize: 15,
-                                        color: ColorManager.desColor,
+                                    children: [
+                                      TextSpan(
+                                        text: 'المظهر \t\t\t\t',
                                       ),
-                                    ),
-                                  ],
+                                      TextSpan(
+                                        text: 'light',
+                                        style: getMediumInterStyle(
+                                          fontSize: 15,
+                                          color: ColorManager.desColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              Switch(
-                                thumbColor: MaterialStatePropertyAll<Color>(
-                                    ColorManager.baseColorLight),
-                                inactiveTrackColor: Colors.grey,
-                                trackOutlineColor:
-                                    MaterialStatePropertyAll<Color>(
-                                        ColorManager.baseColorLight),
-                                value: true,
-                                trackColor: MaterialStatePropertyAll<Color>(
-                                    Colors.white),
-                                onChanged: (bool value) {},
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                                Switch(
+                                  thumbColor: MaterialStatePropertyAll<Color>(
+                                      ColorManager.baseColorLight),
+                                  inactiveTrackColor: Colors.grey,
+                                  trackOutlineColor:
+                                      MaterialStatePropertyAll<Color>(
+                                          ColorManager.baseColorLight),
+                                  value: true,
+                                  trackColor: MaterialStatePropertyAll<Color>(
+                                      Colors.white),
+                                  onChanged: (bool value) {},
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              } else if (state is ProfileErrorState) {
-                return Center(
-                    child: Text('Error fetching profile${state.message}'));
-              } else {
-                return const Center(child: Text('Initial state'));
-              }
-            },
-          )),
+                  );
+                } else if (state is ProfileErrorState) {
+                  return Center(
+                      child: Text('Error fetching profile${state.message}'));
+                } else {
+                  return const Center(child: Text('Initial state'));
+                }
+              },
+            )),
+      ),
     );
   }
 
@@ -168,8 +182,8 @@ class ProfileView extends StatelessWidget {
           BoxDecoration(color: color, borderRadius: BorderRadius.circular(15)),
       child: RichText(
         text: TextSpan(
-          style: getBoldSegoeStyle(
-            fontSize: 25,
+          style: getSemiBoldInterStyle(
+            fontSize: AppSize.s20.sp,
             color: ColorManager.black,
           ),
           children: [
